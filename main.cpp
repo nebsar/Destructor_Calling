e9f46f9d28738d07e97cec6d24c175f92de60918
Eagleye <Eagleye@EagleEye>
2018-09-14 02:41:28 -0400

    Destructor_Calling

--- main.cpp
+++ main.cpp
@@ -13,9 +13,53 @@
 
 #include <iostream>
 
+class Base {
+    int a;
+public:
 
+    Base() {
+        std::cout << "Base Constructor is called\n";
+    }
+
+    virtual ~Base() {
+        std::cout << "Base Destructor is called\n";
+    }
+};
+
+class Derived : public Base {
+public:
+
+    Derived() {
+        std::cout << "Derived Constructor is called\n";
+    }
+
+    virtual ~Derived() {
+        std::cout << "Derived Destructor is called\n";
+    }
+};
 
 int main() {
+    //// Normal csse
+    std::cout << "Normal Case\n";
+    Base* obj = new Base();
+    delete obj;
+
+    ///////placement new case:
+    std::cout << "placement new Case\n";
+    char* memory = new char[10 * sizeof (Base)];
+
+    Base* obj1 = new (&memory[0]) Base();
+    Base* obj2 = new (&memory[4]) Base();
+    Base* obj3 = new (&memory[8]) Base();
+
+    obj1->~Base();
+    obj2->~Base();
+    obj3->~Base();
+
+    delete memory;
+
+
+
 
     return 0;
 }
