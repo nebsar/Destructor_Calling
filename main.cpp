89c1c10d88de2f6cace96df86771a9f4f74d10d6
Eagleye <Eagleye@EagleEye>
2018-09-14 02:20:12 -0400

    Destructor_Calling

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Eagleye
+ *
+ * Created on September 14, 2018, 2:19 AM
+ */
+
+#include <iostream>
+
+
+
+int main() {
+
+    return 0;
+}
+
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=destructor_calling
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/destructor_calling
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=destructorcalling.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/destructorcalling.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=destructor_calling
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/destructor_calling
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=destructorcalling.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/destructorcalling.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
